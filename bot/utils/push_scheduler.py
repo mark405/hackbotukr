ef7108bc6105cb6391a63ae6439aa388e5100cf3
MarkRufin-Zavhorodni <Mark.Rufin-Zavhorodnii@nangasystems.com>
2026-03-07 14:16:52 +0200

    add user input from 3

--- bot/utils/push_scheduler.py
+++ bot/utils/push_scheduler.py
@@ -1,28 +1,50 @@
 import asyncio
+import logging
 import random
 from aiogram import Bot
 from sqlalchemy.future import select
 
 from bot.database.db import SessionLocal
-from bot.database.models import UserProgress
+from bot.database.models import UserProgress, AccessKey
 from bot.utils.push_utils import PUSH_MESSAGES
 from bot.handlers.start import continue_keyboard
 
 
 async def push_loop(bot: Bot):
     while True:
-        await asyncio.sleep(10800)  # 3 години
+        await asyncio.sleep(10800)  # 3 hours
 
-        async with SessionLocal() as session:
-            result = await session.execute(
-                select(UserProgress.telegram_id).where(UserProgress.bot_name == "hackbotukr")
-            )
-            users = result.scalars().all()
+        try:
+            async with SessionLocal() as session:
+                result = await session.execute(
+                    select(UserProgress.telegram_id)
+                    .join(
+                        AccessKey,
+                        AccessKey.telegram_id == UserProgress.telegram_id
+                    )
+                    .where(
+                        UserProgress.bot_name == "hackbotukr",
+                        AccessKey.entered.is_(True)
+                    )
+                )
 
-        for user_id in users:
-            # if user_id != 346457069: continue
-            try:
-                text = random.choice(PUSH_MESSAGES)
-                await bot.send_message(user_id, text, reply_markup=continue_keyboard)
-            except:
-                pass
+                users = result.scalars().all()
+
+            for user_id in users:
+                try:
+                    text = random.choice(PUSH_MESSAGES)
+
+                    await bot.send_message(
+                        user_id,
+                        text,
+                        reply_markup=continue_keyboard
+                    )
+
+                    # небольшая пауза чтобы не словить rate limit
+                    await asyncio.sleep(0.05)
+
+                except Exception as e:
+                    logging.error(f"Push error for {user_id}: {e}")
+
+        except Exception as e:
+            ogging.error(f"Push loop error: {e}")
